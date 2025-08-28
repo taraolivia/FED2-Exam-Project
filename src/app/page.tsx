76e5e06d9912b7f54370fb93e03ef944c5f389de
taraolivia <151462360+taraolivia@users.noreply.github.com>
2025-08-28 22:12:46 +0200

    Working on venues grid componernt - not done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import VenuesGrid from "./components/VenuesGrid";
 
 export default function Home() {
   return (
-    <>
-      <HeroSection />
-      <QuoteSection />
-      <VenuesGrid />
-
-      <main className="">This is the main part of the page</main>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        This is the main part of the page
+      </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
